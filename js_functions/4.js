7f317ff6aeea6bb684678788b2cc173f901da1b4
adnancode78 <adnancode78@gmail.com>
2024-04-16 13:34:46 +0200

    Updat1 4.js
    Updat1 4.js 20240416

--- js_functions/4.js
+++ js_functions/4.js
@@ -1,34 +1,9 @@
-// Importieren des readline-Moduls
-const readline = require('readline');
-
-// Erstellen einer readline-Schnittstelle
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Funktion zum Abfragen des Benutzers
-const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
-
-// Schließen der readline-Schnittstelle beim Beenden des Programms
-rl.on('close', () => process.exit(0));
-
-// Hauptfunktion
-async function main() {
-    // Abrufen der Eingabe des Benutzers
-    const zahl1 = parseInt(await prompt('Geben Sie die erste Zahl ein: '));
-    const zahl2 = parseInt(await prompt('Geben Sie die zweite Zahl ein: '));
-    const zahl3 = parseInt(await prompt('Geben Sie die dritte Zahl ein: '));
-
-    // Berechnen des Mittelwerts
-    const mittelwert = (zahl1 + zahl2 + zahl3) / 3;
-
-    // Ausgabe des Mittelwerts
-    console.log(`Der Mittelwert von ${zahl1}, ${zahl2} und ${zahl3} ist ${mittelwert}.`);
-
-    // Schließen der readline-Schnittstelle
-    rl.close();
+function log(...args) {
+    let line = "";
+    for (let word of args) {
+        line += word + " ";
+    }
+    console.log(line);
 }
 
-// Aufrufen der Hauptfunktion
-main();
+log("Neighbor", "computer");
