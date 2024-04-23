f7d2e21253857f1f8c131f3dc891dca97f97d49b
adnancode78 <adnancode78@gmail.com>
2024-04-23 21:46:15 +0200

    add some change for test git

--- Builtins/Object_Klasse.js
+++ Builtins/Object_Klasse.js
@@ -7,3 +7,6 @@ const car = {
 const object1 = Object.keys(car);
 console.log('Keys of the Car:', object1);
 
+//addition
+const object2 = Object.entries(car);
+console.log('Entries of the Car:', object2);
