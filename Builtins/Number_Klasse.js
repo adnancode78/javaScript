117d85f64a50b35c9036018d1b6cf872f0165d65
adnancode78 <adnancode78@gmail.com>
2024-04-24 04:45:52 +0200

    finished ثءثقexercise of Jason   Number Klasse تم الانتهاء من دالة الرقم

---
+++ Builtins/Number_Klasse.js
@@ -0,0 +1,9 @@
+const convertBin = (binary) => parseInt(binary, 2);
+
+console.log(convertBin("11110111010")); //THANKS W3SCHOOLS.COM
+
+
+var number = 1978;
+console.log("the binary number is :", number.toString(2));
+
+
